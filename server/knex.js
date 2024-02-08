b56363c3ab73795a2e8829fd98699e8cf2f2a423
femogiga <femogiga@yahoo.com>
2024-02-08 14:09:52 +0000

    changed connection knex1

--- server/knex.js
+++ server/knex.js
@@ -8,16 +8,10 @@
 //   },
 //  });
 
+const knex = require('knex')({
+  client: 'pg',
+  connection:
+    'postgresql://postgres:Fbgea44efcbd6*aa*ed*G-*bG5c2A5fe@postgres-e29z.railway.internal:5432/railway',
+});
 
- const knex = require('knex')({
-   client: 'pg',
-   connection: {
-     host: 'monorail.proxy.rlwy.net',
-     user: 'postgres',
-     password: 'Fbgea44efcbd6*aa*ed*G-*bG5c2A5fe',
-     database: 'railway',
-   },
- });
-
-
-module.exports = {knex}
+module.exports = { knex };
