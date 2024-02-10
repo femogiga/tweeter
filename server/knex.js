fb9860f907272a01ebedf91f39426225409415aa
femogiga <femogiga@yahoo.com>
2024-02-10 09:58:02 +0000

    alpha 1.1

--- server/knex.js
+++ server/knex.js
@@ -1,17 +1,17 @@
- const knex = require('knex')({
-  client: 'pg',
-  connection: {
-    host: 'localhost',
-    user: 'postgres',
-    password: 'lara',
-    database: 'tweeter',
-  },
- });
-
-// const knex = require('knex')({
+//  const knex = require('knex')({
 //   client: 'pg',
-//   connection:
-//     'postgresql://postgres:Fbgea44efcbd6*aa*ed*G-*bG5c2A5fe@postgres-e29z.railway.internal:5432/railway',
-// });
+//   connection: {
+//     host: 'localhost',
+//     user: 'postgres',
+//     password: 'lara',
+//     database: 'tweeter',
+//   },
+//  });
+
+const knex = require('knex')({
+  client: 'pg',
+  connection:
+    'postgresql://postgres:Fbgea44efcbd6*aa*ed*G-*bG5c2A5fe@postgres-e29z.railway.internal:5432/railway',
+});
 
 module.exports = { knex };
